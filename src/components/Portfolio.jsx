0cf699d47212b0b9b001008ec6564d915426de28
Mr1-Robot <robotmr969@gmail.com>
2022-09-17 10:26:03 +0300

    Edited Portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -42,46 +42,53 @@ const Portfolio = () => {
         ];
 
         return (
-        <div
-            name="portfolio"
-            className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen p-4"
-        >
-            <div className="max-w-screen-lg mx-auto flex flex-col justify-center w-full h-full">
-            <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-                Portfolio
-                </p>
-                <p className="py-6">Check out some of my work right here</p>
-            </div>
-
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-                {portfolios.map(({ id, src, href }) => (
-                <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-                    <img
-                    src={src}
-                    alt=""
-                    className="rounded-md duration-200 hover:scale-105"
-                    />
-                    <div className="flex items-center justify-center">
-                    <a
-                        href={href}
-                        className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150"
-                    >
-                        Demo
-                    </a>
-                    <a
-                        href="https://Github.com/Mr1-Robot"
-                        className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150"
-                    >
-                        Code
-                    </a>
-                    </div>
+            <div
+                name="portfolio"
+                className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen p-4"
+            >
+                <div className="max-w-screen-lg mx-auto flex flex-col justify-center w-full h-full">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                    Portfolio
+                    </p>
+                    <p className="py-6">Check out some of my work right here</p>
                 </div>
-                ))}
+
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+                    {portfolios.map(({ id, src, href }) => (
+                    <div
+                        key={id}
+                        className="shadow-md shadow-gray-600 rounded-lg"
+                    >
+                        <img
+                        src={src}
+                        alt=""
+                        className="rounded-md duration-200 hover:scale-105"
+                        />
+                        <div className="flex items-center justify-center">
+                        <a
+                            href={href}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150"
+                        >
+                            Demo
+                        </a>
+                        <a
+                            href="https://Github.com/Mr1-Robot"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-150"
+                        >
+                            Code
+                        </a>
+                        </div>
+                    </div>
+                    ))}
+                </div>
+                </div>
             </div>
-            </div>
-        </div>
-    );
+        );
 }
 
 export default Portfolio
